228dfbd7d272551d066a71fe3da264b657a0117f
Sopheap Pit <sopheappit@chhay-lyhour.local>
2025-12-16 11:50:39 +0700

    update the nav

--- src/components/authordash/AuthDashSidebar.jsx
+++ src/components/authordash/AuthDashSidebar.jsx
@@ -73,7 +73,7 @@ const AuthDashSidebar = ({currentUser}) => {
 
         {/* nav */}
         <nav className='mt-[30px] md:mt-[40px] lg:mt-[50px] flex flex-col gap-4 w-full'>
-          <h3 className="text-4xl text-center font-semibold text-[#1A5632] uppercase mt-4">
+          <h3 className="text-2xl text-center font-semibold text-[#1A5632] uppercase mt-4">
             My Content
           </h3>
 
